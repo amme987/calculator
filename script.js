eae68a80fb7808183a8bf1da1875653a661fac8f
amme987 <emmagination97@gmail.com>
2023-03-22 15:20:18 -0400

    Create basic math functions

---
+++ script.js
@@ -0,0 +1,29 @@
+let num1, num2, operator;
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(num1, num2, operator) {
+  if (operator === "+") {
+    add(num1, num2);
+  } else if (operator === "-") {
+    subtract(num1, num2);
+  } else if (operator === "*") {
+    multiply(num1, num2);
+  } else {
+    divide(num1, num2);
+  }
+}
