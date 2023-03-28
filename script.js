fcafe100986c7345b79d74fd26ae5b500d05e468
amme987 <emmagination97@gmail.com>
2023-03-28 11:30:19 -0400

    Calculate two operands

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let num1, num2, operator;
 
 function add(a, b) {
-  return a + b;
+  return Number(a) + Number(b);
 }
 
 function subtract(a, b) {
@@ -18,21 +18,38 @@ function divide(a, b) {
 
 function operate(num1, num2, operator) {
   if (operator === "+") {
-    add(num1, num2);
+    return add(num1, num2);
   } else if (operator === "-") {
-    subtract(num1, num2);
+    return subtract(num1, num2);
   } else if (operator === "*") {
-    multiply(num1, num2);
+    return multiply(num1, num2);
   } else {
-    divide(num1, num2);
+    return divide(num1, num2);
   }
 }
 
 const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
 const numbers = document.querySelectorAll(".number");
-let displayValues;
-numbers.forEach(number => {
-  number.addEventListener("click", () => {
-    displayValues = display.textContent += `${number.getAttribute("id")}`;
+const operators = document.querySelectorAll(".operator");
+let num = "";
+let sum;
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    if (button.getAttribute("class") === "number") {
+      num += button.getAttribute("id");
+      display.textContent = num;
+    } else if (button.getAttribute("class") === "operator") {
+      operator = button.getAttribute("id");
+      if (!num1) {
+        num1 = num;
+      }
+      num = "";
+    } else if (button.getAttribute("id") === "=") {
+      num2 = num;
+      display.textContent = sum = operate(num1, num2, operator);
+      num1 = sum;
+    }
   });
 });
